183f90beaed80dd0c698e23ef6d96b15000ff3fc
zaelanirizal02 <zaelanirizal02@gmail.com>
2019-10-31 00:14:17 +0700

    ini

---
+++ app/src/main/java/com/example/loginbiasa/Welcome.java
@@ -0,0 +1,27 @@
+package com.example.loginbiasa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+
+    Button btnExit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //perintah untuk mengakhiri aplikasi
+                finish();
+            }
+        });
+
+    }
+}
